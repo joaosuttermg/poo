8c6a0472e546db544d156b511ffce8a7f5dfeaf2
joao sutter <joaosutter43@gmail.com>
2024-06-27 21:54:18 -0300

    exercicio classes

---
+++ classes/src/main/java/com/poo/classes/Endereco.java
@@ -0,0 +1,5 @@
+package com.poo.classes;
+
+public class Endereco {
+
+}
---
+++ classes/src/main/java/com/poo/classes/Pessoa.java
@@ -0,0 +1,11 @@
+package com.poo.classes;
+
+public class Pessoa {
+    int pessoaid;
+    int rg;
+    int cpf;
+    String nome;
+    int enderecoid;
+
+
+}
