b9bb96499557aa1a575d38cdc7b7d6fff372f27e
joao sutter <joaosutter43@gmail.com>
2024-07-02 18:30:37 -0300

    Classes

--- classes/src/main/java/com/poo/classes/Pessoa.java
+++ classes/src/main/java/com/poo/classes/Pessoa.java
@@ -1,11 +1,31 @@
 package com.poo.classes;
 
 public class Pessoa {
-    int pessoaid;
-    int rg;
-    int cpf;
-    String nome;
-    int enderecoid;
+    private int pessoaid;
+    private int rg;
+    private String cpf;
+    private String nome;
+    private int enderecoid;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public int getRg(){
+        return rg;
+    }
+    public void setRg(int rg){
+        this.rg = rg;
+    }
+
 
 
 }
